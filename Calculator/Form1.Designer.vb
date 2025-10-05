456a1d2c5d7dd74b4425bcff7cb1e3fbd01a2acf
Rizalsatriawan <rizalsatriawan08@gmail.com>
2025-10-05 14:37:35 +0700

    Add project files.

---
+++ Calculator/Form1.Designer.vb
@@ -0,0 +1,272 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnbackspace = New Button()
+        btnc = New Button()
+        btntambah = New Button()
+        btnkurang = New Button()
+        btn2 = New Button()
+        btn3 = New Button()
+        btn8 = New Button()
+        btn9 = New Button()
+        btnbagi = New Button()
+        btnkali = New Button()
+        btn5 = New Button()
+        btn6 = New Button()
+        btnhasil = New Button()
+        btn4 = New Button()
+        btn1 = New Button()
+        btn0 = New Button()
+        btn7 = New Button()
+        txtdisplay = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btnbackspace
+        ' 
+        btnbackspace.Font = New Font("Segoe UI", 18.0F, FontStyle.Bold)
+        btnbackspace.Location = New Point(209, 125)
+        btnbackspace.Name = "btnbackspace"
+        btnbackspace.Size = New Size(67, 61)
+        btnbackspace.TabIndex = 0
+        btnbackspace.Text = "🗑"
+        btnbackspace.UseVisualStyleBackColor = True
+        ' 
+        ' btnc
+        ' 
+        btnc.Font = New Font("Segoe UI", 18.0F, FontStyle.Bold)
+        btnc.Location = New Point(123, 125)
+        btnc.Name = "btnc"
+        btnc.Size = New Size(67, 61)
+        btnc.TabIndex = 1
+        btnc.Text = "C"
+        btnc.UseVisualStyleBackColor = True
+        ' 
+        ' btntambah
+        ' 
+        btntambah.Font = New Font("Segoe UI", 18.0F, FontStyle.Bold)
+        btntambah.Location = New Point(293, 208)
+        btntambah.Name = "btntambah"
+        btntambah.Size = New Size(67, 61)
+        btntambah.TabIndex = 3
+        btntambah.Text = "+"
+        btntambah.UseVisualStyleBackColor = True
+        ' 
+        ' btnkurang
+        ' 
+        btnkurang.Font = New Font("Segoe UI", 18.0F, FontStyle.Bold)
+        btnkurang.Location = New Point(293, 125)
+        btnkurang.Name = "btnkurang"
+        btnkurang.Size = New Size(67, 61)
+        btnkurang.TabIndex = 2
+        btnkurang.Text = "-"
+        btnkurang.UseVisualStyleBackColor = True
+        ' 
+        ' btn2
+        ' 
+        btn2.Font = New Font("Segoe UI", 18.0F, FontStyle.Bold)
+        btn2.Location = New Point(123, 208)
+        btn2.Name = "btn2"
+        btn2.Size = New Size(67, 61)
+        btn2.TabIndex = 5
+        btn2.Text = "2"
+        btn2.UseVisualStyleBackColor = True
+        ' 
+        ' btn3
+        ' 
+        btn3.Font = New Font("Segoe UI", 18.0F, FontStyle.Bold)
+        btn3.Location = New Point(209, 208)
+        btn3.Name = "btn3"
+        btn3.Size = New Size(67, 61)
+        btn3.TabIndex = 4
+        btn3.Text = "3"
+        btn3.UseVisualStyleBackColor = True
+        ' 
+        ' btn8
+        ' 
+        btn8.Font = New Font("Segoe UI", 18.0F, FontStyle.Bold)
+        btn8.Location = New Point(123, 372)
+        btn8.Name = "btn8"
+        btn8.Size = New Size(67, 61)
+        btn8.TabIndex = 11
+        btn8.Text = "8"
+        btn8.UseVisualStyleBackColor = True
+        ' 
+        ' btn9
+        ' 
+        btn9.Font = New Font("Segoe UI", 18.0F, FontStyle.Bold)
+        btn9.Location = New Point(209, 372)
+        btn9.Name = "btn9"
+        btn9.Size = New Size(67, 61)
+        btn9.TabIndex = 10
+        btn9.Text = "9"
+        btn9.UseVisualStyleBackColor = True
+        ' 
+        ' btnbagi
+        ' 
+        btnbagi.Font = New Font("Segoe UI", 18.0F, FontStyle.Bold)
+        btnbagi.Location = New Point(293, 372)
+        btnbagi.Name = "btnbagi"
+        btnbagi.Size = New Size(67, 61)
+        btnbagi.TabIndex = 9
+        btnbagi.Text = ":"
+        btnbagi.UseVisualStyleBackColor = True
+        ' 
+        ' btnkali
+        ' 
+        btnkali.Font = New Font("Segoe UI", 18.0F, FontStyle.Bold)
+        btnkali.Location = New Point(293, 289)
+        btnkali.Name = "btnkali"
+        btnkali.Size = New Size(67, 61)
+        btnkali.TabIndex = 8
+        btnkali.Text = "x"
+        btnkali.UseVisualStyleBackColor = True
+        ' 
+        ' btn5
+        ' 
+        btn5.Font = New Font("Segoe UI", 18.0F, FontStyle.Bold)
+        btn5.Location = New Point(123, 289)
+        btn5.Name = "btn5"
+        btn5.Size = New Size(67, 61)
+        btn5.TabIndex = 7
+        btn5.Text = "5"
+        btn5.UseVisualStyleBackColor = True
+        ' 
+        ' btn6
+        ' 
+        btn6.Font = New Font("Segoe UI", 18.0F, FontStyle.Bold)
+        btn6.Location = New Point(209, 289)
+        btn6.Name = "btn6"
+        btn6.Size = New Size(67, 61)
+        btn6.TabIndex = 6
+        btn6.Text = "6"
+        btn6.UseVisualStyleBackColor = True
+        ' 
+        ' btnhasil
+        ' 
+        btnhasil.Font = New Font("Segoe UI", 18.0F, FontStyle.Bold)
+        btnhasil.Location = New Point(123, 454)
+        btnhasil.Name = "btnhasil"
+        btnhasil.Size = New Size(237, 61)
+        btnhasil.TabIndex = 17
+        btnhasil.Text = "="
+        btnhasil.UseVisualStyleBackColor = True
+        ' 
+        ' btn4
+        ' 
+        btn4.Font = New Font("Segoe UI", 18.0F, FontStyle.Bold)
+        btn4.Location = New Point(38, 289)
+        btn4.Name = "btn4"
+        btn4.Size = New Size(67, 61)
+        btn4.TabIndex = 16
+        btn4.Text = "4"
+        btn4.UseVisualStyleBackColor = True
+        ' 
+        ' btn1
+        ' 
+        btn1.Font = New Font("Segoe UI", 18.0F, FontStyle.Bold)
+        btn1.Location = New Point(38, 208)
+        btn1.Name = "btn1"
+        btn1.Size = New Size(67, 61)
+        btn1.TabIndex = 15
+        btn1.Text = "1"
+        btn1.UseVisualStyleBackColor = True
+        ' 
+        ' btn0
+        ' 
+        btn0.Font = New Font("Segoe UI", 18.0F, FontStyle.Bold)
+        btn0.Location = New Point(38, 454)
+        btn0.Name = "btn0"
+        btn0.Size = New Size(67, 61)
+        btn0.TabIndex = 13
+        btn0.Text = "0"
+        btn0.UseVisualStyleBackColor = True
+        ' 
+        ' btn7
+        ' 
+        btn7.Font = New Font("Segoe UI", 18.0F, FontStyle.Bold)
+        btn7.Location = New Point(38, 372)
+        btn7.Name = "btn7"
+        btn7.Size = New Size(67, 61)
+        btn7.TabIndex = 12
+        btn7.Text = "7"
+        btn7.UseVisualStyleBackColor = True
+        ' 
+        ' txtdisplay
+        ' 
+        txtdisplay.Font = New Font("Segoe UI", 18.0F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        txtdisplay.Location = New Point(38, 41)
+        txtdisplay.Multiline = True
+        txtdisplay.Name = "txtdisplay"
+        txtdisplay.Size = New Size(322, 59)
+        txtdisplay.TabIndex = 18
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10.0F, 25.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(413, 536)
+        Controls.Add(txtdisplay)
+        Controls.Add(btnhasil)
+        Controls.Add(btn4)
+        Controls.Add(btn1)
+        Controls.Add(btn0)
+        Controls.Add(btn7)
+        Controls.Add(btn8)
+        Controls.Add(btn9)
+        Controls.Add(btnbagi)
+        Controls.Add(btnkali)
+        Controls.Add(btn5)
+        Controls.Add(btn6)
+        Controls.Add(btn2)
+        Controls.Add(btn3)
+        Controls.Add(btntambah)
+        Controls.Add(btnkurang)
+        Controls.Add(btnc)
+        Controls.Add(btnbackspace)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnbackspace As Button
+    Friend WithEvents btnc As Button
+    Friend WithEvents btntambah As Button
+    Friend WithEvents btnkurang As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnbagi As Button
+    Friend WithEvents btnkali As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btnhasil As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents txtdisplay As TextBox
+
+End Class
---
+++ Calculator/Form1.vb
@@ -0,0 +1,128 @@
+﻿Public Class Form1
+
+    Dim angkapertama As Double
+    Dim angkakedua As Double
+    Dim operasi As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtdisplay.TextAlign = HorizontalAlignment.Left
+        txtdisplay.Multiline = True
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        txtdisplay.Text &= "0"
+    End Sub
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        txtdisplay.Text &= "1"
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        txtdisplay.Text &= "2"
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        txtdisplay.Text &= "3"
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        txtdisplay.Text &= "4"
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        txtdisplay.Text &= "5"
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        txtdisplay.Text &= "6"
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        txtdisplay.Text &= "7"
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        txtdisplay.Text &= "8"
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        txtdisplay.Text &= "9"
+    End Sub
+
+    Private Sub btnc_Click(sender As Object, e As EventArgs) Handles btnc.Click
+        txtdisplay.Clear()
+        angkapertama = 0
+        angkakedua = 0
+        operasi = ""
+    End Sub
+
+    Private Sub btnbackspace_Click(sender As Object, e As EventArgs) Handles btnbackspace.Click
+        If txtdisplay.Text.Length > 0 Then
+            txtdisplay.Text = txtdisplay.Text.Substring(0, txtdisplay.Text.Length - 1)
+        End If
+    End Sub
+
+    Private Sub btntambah_Click(sender As Object, e As EventArgs) Handles btntambah.Click
+        angkapertama = Val(txtdisplay.Text)
+        txtdisplay.Clear()
+        operasi = "+"
+    End Sub
+
+    Private Sub btnkurang_Click(sender As Object, e As EventArgs) Handles btnkurang.Click
+        angkapertama = Val(txtdisplay.Text)
+        txtdisplay.Clear()
+        operasi = "-"
+    End Sub
+
+    Private Sub btnkali_Click(sender As Object, e As EventArgs) Handles btnkali.Click
+        angkapertama = Val(txtdisplay.Text)
+        txtdisplay.Clear()
+        operasi = "*"
+    End Sub
+
+    Private Sub btnbagi_Click(sender As Object, e As EventArgs) Handles btnbagi.Click
+        angkapertama = Val(txtdisplay.Text)
+        txtdisplay.Clear()
+        operasi = "/"
+    End Sub
+
+    Private Sub btnhasil_Click(sender As Object, e As EventArgs) Handles btnhasil.Click
+        Try
+            angkakedua = Val(txtdisplay.Text)
+            Select Case operasi
+                Case "+"
+                    txtdisplay.Text = angkapertama + angkakedua
+                Case "-"
+                    txtdisplay.Text = angkapertama - angkakedua
+                Case "*"
+                    txtdisplay.Text = angkapertama * angkakedua
+                Case "/"
+                    If angkakedua = 0 Then
+                        MessageBox.Show("Error: Division by zero is not allowed.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                        txtdisplay.Clear()
+                    Else
+                        txtdisplay.Text = angkapertama / angkakedua
+                    End If
+                Case Else
+                    MessageBox.Show("Please select an operation.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            End Select
+        Catch ex As Exception
+            MessageBox.Show("An error occurred: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles btnkurang.Click
+
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles btn1.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btntambah.Click
+
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles btn5.Click
+
+    End Sub
+End Class
